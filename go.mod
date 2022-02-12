fefa1a436e297472834eda0b4934f864508619a1
tsheringL <tsheringlhamo.gcit@rub.edu.bt>
2022-02-12 22:24:34 +0600

    oauth library added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tsheringL/bookstore_oauth_go
+
+go 1.17
+
+require github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
---
+++ oauth/errors/errors.go
@@ -0,0 +1,40 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string
+	Status  int
+	Error   string
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal server error",
+	}
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
